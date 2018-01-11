42d42c36a4883e74e52f17ad9da420105b525fbd
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2018-01-11 11:12:57 +0100

    init

---
+++ app/src/main/java/fr/enssat/berrou_couillard/interfaces_riches/MainActivity.java
@@ -0,0 +1,28 @@
+package fr.enssat.berrou_couillard.interfaces_riches;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private VideoView vidView;
+    private MediaController vidControl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Vidéo view
+        vidView = (VideoView)findViewById(R.id.videoView);
+        // Par défault on met la première vidéo
+        vidView.setVideoURI(Uri.parse(currentMovie.getUrl()));
+        vidView.start();
+        // Activer Vidéo Control
+        vidControl = new MediaController(this);
+        vidControl.setAnchorView(vidView);
+        vidView.setMediaController(vidControl);
+    }
+}
